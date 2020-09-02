1affc345176fb8efed7f7d685b6d504e253b8cf5
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-09-02 15:51:12 +0200

    EDIT*: Main layout

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,37 @@
     <header>
       <h1>VEGA</h1>
     </header>
-    <nav id="nav">
+
+    <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/library">Library</router-link> |
       <router-link to="/configuration">Configuration</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+
     <router-view />
+
     <footer>Version Alpha 0.1.0</footer>
   </main>
 </template>
+
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+
+  header {
+    text-align: center;
+  }
+
+  nav {
+    text-align: center;
+  }
+
+  footer {
+    text-align: end;
+  }
+}
+</style>
