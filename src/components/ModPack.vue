381d0309e7e32cf70324e7c511fc8740f9fe036e
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-09-02 15:53:44 +0200

    ADD*: ModPack component

---
+++ src/components/ModPack.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="mod-pack">
+    <h3>{{ name }}</h3>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "ModPack",
+  props: {
+    name: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mod-pack {
+  border: solid black 1px;
+  width: 150px;
+}
+</style>
