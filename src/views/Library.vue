44eb5b18f73b542ef0ec048d107854ae825cf4c9
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-09-07 17:40:29 +0200

    EDIT*: Library view

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -2,51 +2,71 @@
   <section class="library container">
     <h2>{{ $t("views.library.title") }}</h2>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Mod name</th>
-          <th>File extension</th>
-          <th>Mod type</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
+    <section class="mods-list-panel">
+      <ModsList :mods="mods" />
+      <aside class="actions">
+        <form @submit.prevent>
+          <label for="search-mod">Search</label>
+          <input id="search-mod" type="text" />
 
-      <tbody>
-        <tr>
-          <td>Project brutality</td>
-          <td>p3k</td>
-          <td>
-            <select name="type">
-              <option value="volvo">Full mofication</option>
-              <option value="saab">Maps</option>
-              <option value="mercedes">Small mod</option>
-              <option value="mercedes">Ressources</option>
-              <option value="mercedes">Patch</option>
-            </select>
-          </td>
-          <td>
-            <button>Edit</button>
-            <button>Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          <fieldset>
+            <legend>Show only</legend>
+
+            <input id="wad-file" type="checkbox" name="file-type" />
+            <label for="wad-file">wad</label><br />
+
+            <input id="wad-zip" type="checkbox" name="file-type" />
+            <label for="wad-zip">zip</label><br />
+
+            <input id="wad-pk3" type="checkbox" name="file-type" />
+            <label for="wad-pk3">pk3</label><br />
+
+            <input id="wad-pk7" type="checkbox" name="file-type" />
+            <label for="wad-pk7">pk7</label>
+          </fieldset>
+        </form>
+      </aside>
+    </section>
   </section>
 </template>
 
+<script>
+import { mapState } from "vuex";
+import { readDir } from "tauri/api/fs";
+import ModsList from "@/components/ModsList.vue";
+
+export default {
+  name: "Library",
+  components: {
+    ModsList
+  },
+  data() {
+    return {
+      mods: []
+    };
+  },
+  computed: {
+    ...mapState({ modsFolderPath: state => state.settings.modsFolderPath })
+  },
+  async mounted() {
+    if (this.modsFolderPath !== 0) {
+      this.mods = await readDir(this.modsFolderPath);
+    } else {
+      this.mods = []
+    }
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .library {
-  table {
-    border: black solid 2px;
+  .mods-list-panel {
+    display: flex;
 
-    thead {
-      background-color: dimgray;
-      color: white;
-    }
-
-    td {
-      border: black solid 1px;
+    .actions {
+      margin-left: 10px;
+      display: flex;
+      flex-direction: column;
     }
   }
 }
