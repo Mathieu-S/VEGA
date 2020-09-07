412400d5a9569419a19f418330494497c2046433
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-09-07 13:44:15 +0200

    ADD*: Engine path and mods folder path in Configuration view

--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -5,42 +5,74 @@
     <form @submit.prevent>
       <label for="language">Language :</label>
       <select id="language" v-model="$i18n.locale">
-        <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">
+        <option
+          v-for="(lang, i) in settings.langs"
+          :key="`Lang${i}`"
+          :value="lang"
+        >
           {{ lang }}
         </option>
       </select>
 
-      <label for="gzdoom-path">GZDoom path :</label>
-      <input id="gzdoom-path" v-model="gzDoomPath" type="text" />
-      <button @click="setGzdoomPath">Set path to GZDoom</button>
+      <label for="doom-engine-path">GZDoom path :</label>
+      <input
+        id="doom-engine-path"
+        v-model="settings.doomEnginePath"
+        type="text"
+      />
+      <button @click="setDoomEnginePath">Set path to GZDoom</button>
 
       <label for="mods-path">Mods folder path :</label>
-      <input id="mods-path" v-model="modsFolderPatch" type="text" />
-      <button @click="setModFolder">Set path to mod folder</button>
+      <input id="mods-path" v-model="settings.modsFolderPath" type="text" />
+      <button @click="setModsFolderPath">Set path to mod folder</button>
 
-      <button>Save settings</button>
+      <button @click="saveConfiguration">Save settings</button>
     </form>
   </section>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 import { open } from "tauri/api/dialog";
 
 export default {
   name: "Configuration",
   data() {
     return {
-      langs: ["en", "fr"],
-      gzDoomPath: "",
-      modsFolderPatch: ""
+      settings: {
+        langs: ["en", "fr"],
+        doomEnginePath: "",
+        modsFolderPath: ""
+      }
     };
   },
+  computed: {
+    ...mapState({
+      doomEnginePath: state => state.settings.doomEnginePath,
+      modsFolderPath: state => state.settings.modsFolderPath
+    })
+  },
+  mounted() {
+    this.settings.doomEnginePath = this.doomEnginePath;
+    this.settings.modsFolderPath = this.modsFolderPath;
+  },
   methods: {
-    async setGzdoomPath() {
-      this.gzDoomPath = await open();
+    ...mapActions("settings", ["setPath"]),
+    async setDoomEnginePath() {
+      this.settings.doomEnginePath = await open();
     },
-    async setModFolder() {
-      this.modsFolderPatch = await open({ directory: true });
+    async setModsFolderPath() {
+      this.settings.modsFolderPath = await open({ directory: true });
+    },
+    saveConfiguration() {
+      this.setPath({
+        path: this.settings.doomEnginePath,
+        type: "doomEnginePath"
+      });
+      this.setPath({
+        path: this.settings.modsFolderPath,
+        type: "modsFolderPath"
+      });
     }
   }
 };
